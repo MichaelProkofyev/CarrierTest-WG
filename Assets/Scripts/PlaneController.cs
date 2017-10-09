08a803ac4aa6a039c65705b2c8902c8b380a1422
Michael Prokofyev <mikeprokofyev@gmail.com>
2017-10-09 07:57:28 +0300

    init

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour {
+
+    public float maxAngularVelocity = 60;
+     
+    Transform target;
+
+    float maxVelocity = 1f;
+
+    Vector2 velocity = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+        target = ShipMovement.Instance.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 desired_velocity = (target.position - transform.position).normalized * maxVelocity;
+        Vector2 steering = (desired_velocity - velocity).normalized * (maxAngularVelocity * Time.deltaTime);
+        //print(Vector2.Angle(desired_velocity, velocity));
+
+
+        velocity += steering;
+        transform.position += (Vector3)velocity * Time.deltaTime;
+
+        //Rotate to face target
+        Vector2 frameTarget = velocity - (Vector2)transform.position; //?????
+        float rot_z = Mathf.Atan2(desired_velocity.y, desired_velocity.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+}
---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : SingletonComponent<ShipMovement> {
+    
+    float maxRotationForce = 1f;
+    float maxAcceleration = 15f;
+
+
+    float speedIncrement = 10f;
+    float rotationIncrement = 2f;
+
+    Rigidbody2D rb;
+    float acceleration;
+    float rotationForce;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponentInChildren<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        if (Input.GetButtonDown("SpeedUp"))
+        {
+            acceleration += speedIncrement;
+            acceleration = Mathf.Clamp(acceleration, -maxAcceleration, maxAcceleration);
+
+        }
+        else if (Input.GetButtonDown("SpeedDown"))
+        {
+            acceleration -= speedIncrement;
+            acceleration = Mathf.Clamp(acceleration, -maxAcceleration, maxAcceleration);
+
+        }
+        else if (Input.GetButtonDown("MakePlane"))
+        {
+            print("making plane");
+        }
+
+        if (Input.GetButton("TurnLeft"))
+        {
+            rotationForce += rotationIncrement * Time.deltaTime;
+            rotationForce = Mathf.Clamp(rotationForce, -maxRotationForce, maxRotationForce);
+        }
+        else if (Input.GetButton("TurnRight"))
+        {
+            rotationForce -= rotationIncrement * Time.deltaTime;
+            rotationForce = Mathf.Clamp(rotationForce, -maxRotationForce, maxRotationForce);
+
+        }
+
+    }
+
+    void FixedUpdate() 
+    {
+        rb.AddRelativeForce(Vector2.up * acceleration * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        //rb.AddTorque(rotationForce * rb.velocity.magnitude, ForceMode2D.Impulse);
+        rb.MoveRotation(rb.rotation + rotationForce * rb.velocity.magnitude*.5f);
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(5 , 5, 200, 25), "velocity " + rb.velocity);
+        GUI.Box(new Rect(5, 30, 200, 25), "acceleration " + acceleration);
+        GUI.Box(new Rect(5, 55, 200, 25), "rotation force " + rotationForce);
+
+
+    }
+}
